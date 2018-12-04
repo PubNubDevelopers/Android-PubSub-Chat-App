cb3f44833f2928775d20954c3d72c6507e16a018
Syed Ahmed <syed@syed-ahmed-2.local>
2018-12-04 14:49:56 -0800

    bug fixes

--- app/src/main/java/me/nxsyed/androidpubsub/MainActivity.kt
+++ app/src/main/java/me/nxsyed/androidpubsub/MainActivity.kt
@@ -2,8 +2,10 @@ package me.nxsyed.androidpubsub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.gson.JsonParser
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import com.pubnub.api.PNConfiguration
 import com.pubnub.api.PubNub
 import com.pubnub.api.models.consumer.PNStatus
@@ -12,26 +14,29 @@ import java.util.*
 import com.pubnub.api.models.consumer.PNPublishResult
 import com.pubnub.api.callbacks.PNCallback
 import com.pubnub.api.callbacks.SubscribeCallback
-import com.pubnub.api.enums.PNStatusCategory
 import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult
 
 class MainActivity : AppCompatActivity() {
 
-     var subscribeCallback: SubscribeCallback = object : SubscribeCallback() {
-        override fun status(pubnub: PubNub, status: PNStatus) {
-
-        }
-        override fun message(pubnub: PubNub, message: PNMessageResult) {
-            println(message.message.toString())
-        }
-        override fun presence(pubnub: PubNub, presence: PNPresenceEventResult) {
-        }
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val publishText = findViewById<EditText>(R.id.editTextPublish)
+        val subscribeText = findViewById<TextView>(R.id.textViewSubscribe)
+
+        var subscribeCallback: SubscribeCallback = object : SubscribeCallback()  {
+            override fun status(pubnub: PubNub, status: PNStatus) {
+
+            }
+            override fun message(pubnub: PubNub, message: PNMessageResult) {
+                runOnUiThread {
+                    subscribeText.text = message.message.toString()
+                }
+            }
+            override fun presence(pubnub: PubNub, presence: PNPresenceEventResult) {
+            }
+        }
 
         val pnConfiguration = PNConfiguration()
         pnConfiguration.subscribeKey = "sub-c-87dbd99c-e470-11e8-8d80-3ee0fe19ec50"
@@ -39,22 +44,34 @@ class MainActivity : AppCompatActivity() {
         pnConfiguration.secretKey = "true"
         val pubNub = PubNub(pnConfiguration)
 
+        pubNub.run {
+            addListener(subscribeCallback)
+            subscribe()
+                    .channels(Arrays.asList("whiteboard")) // subscribe to channels
+                    .execute()
+        }
 
-        pubNub.publish()
-                .message("Here is a message")
-                .channel("whiteboard")
-                .async(object : PNCallback<PNPublishResult>() {
-                    override fun onResponse(result: PNPublishResult, status: PNStatus) {
-                        if (!status.isError) {
-                            println("pub timetoken: " + result.timetoken!!)
-                        }
-                        println("pub status code: " + status.statusCode)
-                    }
-                })
-        pubNub.addListener(subscribeCallback)
-        pubNub.subscribe()
-                .channels(Arrays.asList("whiteboard")) // subscribe to channels
-                .execute()
+        publishText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                pubNub.run {
+                    publish()
+                            .message(publishText.text.toString())
+                            .channel("whiteboard")
+                            .async(object : PNCallback<PNPublishResult>() {
+                                override fun onResponse(result: PNPublishResult, status: PNStatus) {
+                                    if (!status.isError) {
+                                        println("Message was published")
+                                    }else {
+                                        println("Could not publish")
+                                    }
+                                }
+                            })
+                }
+                return@OnKeyListener true
+            }
+            false
+        })
+
 
     }
 
